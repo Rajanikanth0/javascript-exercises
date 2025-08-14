a0abf0c4d6831b5489c79ae3a1223497b88f5325
Rajanikanth0 <61695662+Rajanikanth0@users.noreply.github.com>
2025-08-14 19:24:23 +0530

    fix: Use normal for-loop

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+  let result = string;
 
+  for (let n = 0; n < num - 1; n++) {
+    result += string;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
