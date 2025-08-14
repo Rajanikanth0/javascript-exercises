32c6856542a1c27a5854053ae55e86f5e9d308d7
Rajanikanth0 <61695662+Rajanikanth0@users.noreply.github.com>
2025-08-14 19:41:05 +0530

    fix: Pass all test

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,11 +1,11 @@
 const repeatString = function(string, num) {
-  if (num < 0) return "INVALID SIZE ERROR";
+  if (num < 0) return "ERROR";
 
   let result = "";
   for (let n = 0; n < num; n++) {
     result += string;
   }
-  
+
   return result;
 };
 
