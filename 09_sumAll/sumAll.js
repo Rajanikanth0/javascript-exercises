b8f07f9a80d61205c9bf5854ebb6e574629bb19d
Rajanikanth0 <61695662+Rajanikanth0@users.noreply.github.com>
2025-08-20 12:07:07 +0530

    fix: Remove identical codes

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,19 +1,16 @@
-const sumAll = function(n1, n2) {
+const sumAll = function(start, end) {
   // Return 'ERROR' for negative numbers
-  if ( (n1 < 0) || (n2 < 0) ) return 'ERROR';
+  if ( (start < 0) || (end < 0) ) return 'ERROR';
   // Return 'ERROR' FOR decimal numbers
-  if (!Number.isInteger(n1) || !Number.isInteger(n2)) return "ERROR";
+  if (!Number.isInteger(start) || !Number.isInteger(end)) return "ERROR";
+  // set start and end with lower and higher values respectively
+  if (start > end) [end, start] = [start, end];
 
   // Init final result with start
   let result = 0;
-  
-  // Init start and end with lower and higher values respectively
-  let [start, end] = (n1 < n2) ? [n1, n2] : [n2, n1];
-  // Increment end by 1 to include the last number
-  end = end + 1;
 
   // Iterate from start to end
-  for (let n = start; n < end; n++) {
+  for (let n = start; n <= end; n++) {
     // Add n numbers to the result
     result += n;
   }
