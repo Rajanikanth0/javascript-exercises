8db7acb11f2cdb63004321ebc46e423259f02a9c
Rajanikanth0 <61695662+Rajanikanth0@users.noreply.github.com>
2025-08-20 14:48:00 +0530

    feat: Convert using the repective formula

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  const result = (fahrenheit - 32) * 5/9;
+
+  // Round up to a single digit after decimal
+  // Convert to number using +
+  return +result.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  const result = celsius * 9/5 + 32;
+  return +result.toFixed(1);
 };
 
 // Do not edit below this line
